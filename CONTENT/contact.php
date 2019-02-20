2875ce42c8a37e06d258bc49623e5ad6462d2576
NathanLeysGit <nathanley.personal@gmail.com>
2019-02-20 16:30:17 -0600

    Created new folder name

---
+++ CONTENT/contact.php
@@ -0,0 +1,21 @@
+<!--Meta Data======================================================= -->
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>Nathan Ley | Professional Web Developer</title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="shortcut icon" href="../MISC/title.ico">
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans:400,700,800" rel="stylesheet">
+    <link href="../STYLE/home.css" rel="stylesheet" type="text/css">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.2/css/all.css" integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous">
+  </head>
+
+  <body>
+    <div class="container">
+      <form action="<?PHP echo htmlspecialchars($_SERVER["PHP_SELF"]);?>" method="post">
+
+      </form>
+    </div>
+  </body>
+</html>
