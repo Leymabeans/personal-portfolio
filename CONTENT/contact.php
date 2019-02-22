7139a08c855bd79a8b2d112f8e7c8dda37e0244e
NathanLeysGit <nathanley.personal@gmail.com>
2019-02-21 22:20:48 -0600

    Started backend development for contact information

--- CONTENT/contact.php
+++ CONTENT/contact.php
@@ -1,3 +1,29 @@
+<!--Processing for the contact form=================================-->
+<?php
+  //1 Connect to MySQL database
+  $host = "localhost";
+  $username = "root";
+  $password = "root";
+  $database = "personal";
+  $db = mysqli_connect($host, $username, $password, $database) or die('Error connecting to MySQL server');
+
+  //2 Check connection
+  if ($db === false){
+    die("Error: Could not connect to the database" . mysqli_connect_error());   
+  }
+
+  //3 Take inputed information
+  if ($SERVER["REQUEST_METHOD"] == "POST"){
+    $firstname = $_POST["firstname"];
+    $lastname = $_POST["lastname"];
+    $email = $_POST["email"];
+    $message = $_POST["message"];
+  } 
+
+  //4 Take all information from the personal database
+  $query = "SELECT * FROM messengerInfo";
+?>
+
 <!--Meta Data======================================================= -->
 <!DOCTYPE html>
 <html>
@@ -14,7 +40,17 @@
   <body>
     <div class="container">
       <form action="<?PHP echo htmlspecialchars($_SERVER["PHP_SELF"]);?>" method="post">
-
+        <h3>Contact Us Now</h3>
+        <h2>Do you have a question? Email us now!</h2>
+        <input type="text" name="firstname" placeholder="First Name">
+        <br>
+        <input type="text" name="lastname" placeholder="Last Name">
+        <br>
+        <input type="email" name="email" placeholder="Email">
+        <br>
+        <input type="text" name="message" placeholder="Message">
+        <br>
+        <input type="submit">
       </form>
     </div>
   </body>
