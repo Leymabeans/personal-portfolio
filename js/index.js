7f70f217a709f013fc733a2538797ef7b6a0d4a4
NathanLeysGit <nathanley.personal@gmail.com>
2019-07-16 12:26:10 -0500

    Removing opening onclick

--- js/index.js
+++ js/index.js
@@ -10,17 +10,19 @@ function showPage() {
 
 /*Greeting=======================================*/
 function change1() {
-  document.getElementById("message1_1").style.display = "none";
-  document.getElementById('button1_1').style.display = "none";
+  document.getElementById('opening').src = "./images/giddy_opening2.jpg";
 
-  document.getElementById('message1_2').style.display = "block";
+  document.getElementById('button1_1').style.display = "none";
   document.getElementById('button1_2').style.display = "block";
 }
 
 function change2() {
-  document.getElementById("message1_2").style.display = "none";
-  document.getElementById("button1_2").style.display = "none";
+  document.getElementById('opening').src = "./images/giddy_opening3.jpg";
 
-  document.getElementById('message1_3').style.display = "block";
+  document.getElementById("button1_2").style.display = "none";
   document.getElementById('button1_3').style.display = "block";
+}
+
+function disappear() {
+  document.getElementById('greeting').style.display = "none";
 }
