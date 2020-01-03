5308303221639895a48c6acf4eafb3ba4fd4f7c9
NathanLeysGit <nathanley.personal@gmail.com>
2020-01-03 16:15:34 -0600

    File compression and relinking

--- src/js/index.js
+++ src/js/index.js
@@ -1,3 +1,14 @@
+//Create horizontal progress scroll bar----------
+window.onscroll = function() {progressBar()};
+function progressBar() {
+var winScroll = document.body.scrollTop || document.documentElement.scrollTop;
+var height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+var scrolled = (winScroll / height) * 100;
+document.getElementById("bar").style.width = scrolled + "%";
+}
+
+
+//Returns user to previous page (homepage)-------
 function goBack() {
   window.history.back();
 }
